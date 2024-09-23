769cd53d524f4224e9a789bde0519f8746c0323e
1MH-s <mohammad.2003827@gmail.com>
2024-09-23 09:28:21 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notes app',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const Text('Notes app'),
+    );
+  }
+}
