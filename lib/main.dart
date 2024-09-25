98477140bb0528ab427f242fb6c2d0a38eb562a6
Mohammad Sameer <mohammad.2003827@gmail.com>
2024-09-25 19:56:23 +0300

    added hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+
+//Packages:
+//Hive:|||||||||||||
+import 'package:hive_flutter/hive_flutter.dart';
+
+//Views:|||||||||||||||||||||||
+import 'package:notes_app/views/notes_view.dart';
 import 'package:notes_app/views/edit_note_view.dart';
 
-//Views:
-import 'package:notes_app/views/notes_view.dart';
-
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox("notes");
   runApp(const NotesApp());
 }
 
