792eecf2fb69539a21d9a7f21e05561d5aeef2ec
Mohammad Sameer <mohammad.2003827@gmail.com>
2024-09-26 13:35:32 +0300

    created fetchAllNotes in notes_cubit.dart

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,8 +1,23 @@
+//Flutter:||||||||||||||||||
 import 'package:flutter_bloc/flutter_bloc.dart';
+//Packages
+//Hive:|||||||||||||||||||||
+import 'package:hive_flutter/adapters.dart';
+
+//Models:|||||||||||||||||||||||
 import 'package:notes_app/models/note_model.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+  void fetchAllNotes() {
+    //emit(NotesLoading());
+    try {
+      Box<NoteModel> notesBox = Hive.box<NoteModel>("notes");
+      emit(NotesSuccess(notesBox.values.toList()));
+    } catch (exception) {
+      emit(NotesFailure(exception.toString()));
+    }
+  }
 }
